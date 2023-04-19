6ff99a416ae96406aa53325b8af7565d908be856
Darya Pogo <pogorelaja.darya@yandex.ru>
2023-04-19 20:25:45 +0300

    no err class

--- app.js
+++ app.js
@@ -59,9 +59,9 @@ app.use('*', (req, res) => {
   res.status(ERROR_USER).send({ message: 'Not found' });
 });
 
-app.use((err, req, res, next) => {
-  console.log(err.message);
-});
+// app.use((err, req, res, next) => {
+//   console.log(err.message);
+// });
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/mestodb')
